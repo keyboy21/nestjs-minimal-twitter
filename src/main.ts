281c58a3bb33afbb87f80be450b411dcf94339db
yusuf-saydullayev <yusufsaydullayev@gmail.com>
2024-07-13 13:43:10 +0500

    scalar api-reference for better swagger docs

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,22 @@ import { SwaggerModule } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
 import { swaggerConfig } from './configs/swagger.config';
 import { Logger } from '@nestjs/common';
+import { apiReference } from '@scalar/nestjs-api-reference';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Swagger
   const document = SwaggerModule.createDocument(app, swaggerConfig);
+  app.use(
+    '/api',
+    apiReference({
+      theme: 'kepler',
+      spec: {
+        content: document,
+      },
+    }),
+  )
   SwaggerModule.setup('api', app, document);
 
   // Start server listening
